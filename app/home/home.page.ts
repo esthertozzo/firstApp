363c506bc6deab1b4df1eeb70cb71630d9f8d053
Esther Tozzo <tozzo.est@gmail.com>
2025-02-06 12:47:49 -0300

    Add files via upload

---
+++ app/home/home.page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: false,
+})
+export class HomePage {
+
+  constructor() {}
+
+  mensagem = "";
+
+  verMensagem(){
+    this.mensagem = "Bem vindo ao ionic!";
+  }
+
+}
